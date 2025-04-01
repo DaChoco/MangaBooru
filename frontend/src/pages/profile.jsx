9e2e7796931bca1315d01b20a9310b6fbc1b2c53
DaChoko <Mailechokoe2@gmail.com>
2025-04-01 23:32:24 +0200

    added login and register boxes

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -14,26 +14,38 @@ function Profile(){
     const {showLoginBox, setShowLoginBox} = useContext(loggedIn)
     const {showRegisterBox, setShowRegisterBox} = useContext(loggedIn)
 
+    const [emailquery, setEmailquery] = useState("")
+    const [usernamequery, setUsernamequery] = useState("")
+    const [passwordquery, setPasswordquery] = useState("")
+
     function showLogin(){
         if (showRegisterBox === true){
             setShowRegisterBox(!showRegisterBox)
+            
         }
+        
         setShowLoginBox(!showLoginBox)
+        console.log(showLoginBox)
     }
 
     function showRegister(){
         if (showLoginBox === true){
             setShowLoginBox(!showLoginBox)
         }
+        
         setShowRegisterBox(!showRegisterBox)
+        console.log(showRegisterBox)
     }
     return(
 
-        <div className="main-content">
+        <div className="main-content" style={{position: "relative"}}>
             <Topnav></Topnav> 
 
-            {logged === true ?
-             (<div className='profile-page-container'>
+            {logged === false ?
+             (
+             
+            <> 
+             <div className='profile-page-container'>
                 <div className="anouncement-ppage">
                     <h2>You are not currently logged in.</h2>
 
@@ -42,7 +54,51 @@ function Profile(){
                         <li onClick={()=> showRegister()}><h3>Register</h3></li>
                     </ul>
                 </div>
-             </div>):
+             </div>
+
+             {showLoginBox === true && (
+                <form className="userauthbox" id='login' style={{transform: "translateX(200%)"}}> {/*Swoops in from the left - Login */}
+
+                <span>Login with</span> 
+                <div className="userinputs" >
+                
+                    <input type="text" value={emailquery} onChange={(e) => setEmailquery(e.target.value)} placeholder='Type your email or username'/>
+                    <input type="text"  value={passwordquery} onChange={(e) => setPasswordquery(e.target.value)} placeholder='Type your password'/>
+                    
+                    <div style={{display: "flex", flexDirection: "row", margin: "0 auto 0 3rem", width: "75%"}}>
+                    
+                    <input type="checkbox" id='plsremember' value={"Remember me"}/>
+                    <label htmlFor="plsremember">Remember Me</label>
+                    </div>
+                    <button type="submit">Login</button>
+                    <span className='not-txt' onClick={()=> showRegister()}>Not a user yet? Then Register!</span>
+                  
+                </div>
+    
+                </form>
+            )}
+            
+            {showRegisterBox === true && (
+            <form className="userauthbox" id='register' style={{transform: "translateX(-200%)"}} > {/*Swoops in from the right - Register*/}
+                <span>Register With</span>
+
+            <div className="userinputs">
+                <input type="text" value={usernamequery} onChange={(e) => setUsernamequery(e.target.value)} placeholder='Type your username'/>
+                <input type="text" value={emailquery} onChange={(e) => setEmailquery(e.target.value)} placeholder='Type your email'/>
+                <input type="text" value={passwordquery} onChange={(e) => setPasswordquery(e.target.value)} placeholder='Type your password'/>
+                
+                <div style={{display: "flex", flexDirection: "row", margin: "0 auto 0 3rem", width: "75%"}}>
+                    <input type="checkbox" id='plsremember' value={"Remember me"}/>
+                    <label htmlFor="plsremember">Remember Me</label>
+                </div>
+                <button type="submit">Register</button>
+                <span className='not-txt' onClick={()=> showLogin()}>Already a user? Then login!</span>
+            </div>
+
+
+             </form>)}
+            </>
+             ):
 
              (
             <>
@@ -71,7 +127,7 @@ function Profile(){
                 <div className="profile-content">
 
                 <section id="mysig"> 
-                    <span className='user-sig' contentEditable="true" style={{fontFamily: "Verdana, Arial"}}>
+                    <span className='user-sig' style={{fontFamily: "Verdana, Arial"}}>
                         ~<strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, ducimus!</strong>~
                     </span>
                 </section>
@@ -117,13 +173,8 @@ function Profile(){
                 </div>
              </div>  {/*END OF MAIN Profile Page CONTENT */}
 
-             <div className="loginbox"> {/*Swoops in from the left */}
-
-             </div>
-
-             <div className="registerbox"> {/*Swoops in from the right*/}
-
-             </div>
+            
+             
             </> 
            
             ) 
